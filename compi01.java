6e0f832d42f14c749a28b1d56653cb1bfc88d490
gonzaloro1 <gonzaloro1@gmail.com>
2024-07-26 18:45:31 +0200

    uno

---
+++ compi01.java
@@ -0,0 +1,15 @@
+
+public class compi01 {
+
+	public static void main(String[] args) {
+		System.out.println("Alejandro Manuel Patiño García");
+		System.out.println("C/ Xenxides, 19, Oleiros");
+		System.out.println("Riveira");
+		System.out.println("");
+		System.out.println("Gonzalo Rodriguez Osorio");
+		System.out.println("C/ Virxen do Carmen, 32, 4to A");
+		System.out.println("Riveira");
+
+	}
+
+}
